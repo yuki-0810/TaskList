22d51351e328be4183050a6693614cd3ee4ad48a
EC2 Default User <ec2-user@ip-172-31-23-213.us-east-2.compute.internal>
2018-02-21 20:08:27 +0000

    kadai-tasklist-1

---
+++ resources/views/TaskList/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+    <h1>id = {{ $task->id }} のタスクの詳細ページ</h1>
+    
+    <p>{{ $task->content }}</p>
+    
+    {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id]) !!}
+    
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
