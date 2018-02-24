f762387b3fc9221550fb205c8f9e219325deead6
EC2 Default User <ec2-user@ip-172-31-23-213.us-east-2.compute.internal>
2018-02-24 23:31:18 +0000

    Bootstrap Design

--- resources/views/TaskList/edit.blade.php
+++ resources/views/TaskList/edit.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <h1>id: {{ $task->id }} のタスク編集ページ</h1>
-
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
-        {!! Form::label('status', '状況:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
-
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            <h1>id: {{ $task->id }} のタスク編集ページ</h1>
+            
+            <div class="form-group">
+                {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+            </div>
+                {!! Form::label('status', '状況:') !!}
+                {!! Form::text('status',null, ['class' => 'form-control']) !!}
+                
+            <div class="form-group">
+                {!! Form::label('content', 'メッセージ:') !!}
+                {!! Form::text('content',null, ['class' => 'form-control']) !!}
+            </div>
+            
+            {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
--- resources/views/TaskList/show.blade.php
+++ resources/views/TaskList/show.blade.php
@@ -5,13 +5,21 @@
 <!-- ここにページ毎のコンテンツを書く -->
     <h1>id = {{ $task->id }} のタスクの詳細ページ</h1>
     
-    <p>{{ $task->status }}</p>
-    <p>{{ $task->content }}</p>
+        <table class="table table-bordered">
+        <tr>
+            <th>状況</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
     
-    {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
     
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger'] ) !!}
     {!! Form::close() !!}
 
 @endsection
